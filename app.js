1407ca6cb3f997422caf6b217a29f23ebacbd25a
sagar2526 <sagarhinge01@gmail.com>
2018-08-31 15:44:53 +0530

    User model added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connection.on('error', (error) => console.error(error))
+mongoose.connection.on('open', () => console.log("success in connecting to mongodb"))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(4040, () => console.log('Express server at 4040'))
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+var UserSchema = new mongoose.Schema({
+    username:	String,
+    email: String,
+    password: String,
+    password-expiry: timeStamp,
+    first-name: String,
+    middle-name: String,
+    last-name: String,
+    facebook: String,
+    google: String,
+    token: Array,
+    createdAt: timeStamp,
+    updatedAt: timeStamp,
+})
